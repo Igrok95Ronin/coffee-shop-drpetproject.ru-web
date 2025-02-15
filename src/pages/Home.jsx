a19a90b9824235fc7fefb013944cb3a01aefbd11
rivan Dilmaev <xl92@mail.ru>
2025-02-15 12:06:01 +0300

    add: meta tages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 // src/pages/Home.jsx
-import React from 'react'
+import React from "react";
+import HelmetMeta from "../components/HelmetMeta/HelmetMeta";
 
 function Home() {
   return (
     <div>
+      <HelmetMeta
+        title="Главная | Мое приложение"
+        description="Это главная страница"
+        keywords="скидки, акции, распродажа, товары, магазин"
+      />
       <h2>Главная страница (Public)</h2>
       <p>Сюда доступ есть у всех.</p>
     </div>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -1,9 +1,15 @@
 // src/pages/Home.jsx
 import React from "react";
+import HelmetMeta from "../components/HelmetMeta/HelmetMeta";
 
 function Sale() {
   return (
     <div>
+      <HelmetMeta
+        title="Акции | Мое приложение"
+        description="Все актуальные акции на товары"
+        keywords="скидки, акции, распродажа, товары, магазин"
+      />
       <h2>Акции (Public)</h2>
       <p>Сюда доступ есть у всех.</p>
     </div>
